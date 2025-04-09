ade789dd200946e4e9978390a74dd590ebb1e331
HoangVV01 <hoangvietvu01@gmail.com>
2025-04-09 16:32:31 +0700

    Remove all files from main

--- README.md
+++
@@ -1,6 +0,0 @@
-# Limechat
-A simple app using Vercel, Supabase for deployment.
-Its only a single channel chat so all your chats will be seen by everyone.
-
-# How did I create it
-Mainly using Gemini AI, using antd Reactjs for front-end.
--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://fhcqusiuzzhplujlidju.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoY3F1c2l1enpocGx1amxpZGp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwMDIxMDMsImV4cCI6MjA1NzU3ODEwM30.imGkejNqZpVrmTTJX1yda0eR22xK3IAlt72zpP4IJ3E";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
